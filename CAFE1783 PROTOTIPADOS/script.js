fe5ad4d488d828444518bf9691f065e71a0f09c0
galindojuancarlos1783-blip <galindojuancarlos1783@gmail.com>
2026-07-01 20:40:29 -0500

    Delete CAFE1783 PROTOTIPADOS directory

--- CAFE1783 PROTOTIPADOS/script.js
+++
@@ -1,8 +0,0 @@
-function mostrar(id) {
-    const pantallas = document.querySelectorAll('.pantalla');
-    pantallas.forEach(p => p.style.display = 'none');
-    document.getElementById(id).style.display = 'block';
-}
-
-// Mostrar inicio al cargar
-mostrar('inicio');
